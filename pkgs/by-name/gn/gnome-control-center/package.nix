562a79e681df26130264be54f17a8a810be1a805
Jan Tojnar <jtojnar@gmail.com>
2025-03-11 23:24:19 +0000

    gnome-control-center: 48.beta → 48.rc.1
    https://gitlab.gnome.org/GNOME/gnome-control-center/-/compare/48.beta...48.rc.1

--- pkgs/by-name/gn/gnome-control-center/package.nix
+++ pkgs/by-name/gn/gnome-control-center/package.nix
@@ -74,11 +74,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-control-center";
-  version = "48.beta";
+  version = "48.rc.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-control-center/${lib.versions.major finalAttrs.version}/gnome-control-center-${finalAttrs.version}.tar.xz";
-    hash = "sha256-Dvey0wBObrQY/OcIC7GNGyQm+ylhs5/tnCEYurAebCQ=";
+    hash = "sha256-IMroFNkpicoW5Owmixb5Dp8jrvncTFMYl64do1PayIU=";
   };
 
   patches = [
